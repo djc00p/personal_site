c3da1f273df4d0c9023dc61ad2d19b7fdcf683ec
Deonte Cooper <deonte.cooper12@gmail.com>
2019-03-05 19:43:53 -0700

    Added test to verify about page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -14,4 +14,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Page not found.")
     assert_equal 404, page.status_code
   end
+
+  def test_has_an_about_me_page
+    visit '/about'
+
+    assert page.has_content?("About Me!")
+    assert_equal 200, page.status_code
+  end
 end
